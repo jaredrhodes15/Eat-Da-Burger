3635f3575cac2226ab859cd0f3905126886884ab
Jared Rhodes <jaredrhodes15@gmail.com>
2019-03-27 17:39:55 -0600

    Seeds.sql schema uploaded to mysql

--- db/schema.sql
+++ db/schema.sql
@@ -4,7 +4,7 @@ CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers (
-    id INT NOT NULL AUTO_INCREMENT
+    id INT NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(100) NOT NULL,
     devoured BOOLEAN,
     PRIMARY KEY (id)
